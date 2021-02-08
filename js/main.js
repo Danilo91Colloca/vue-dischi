c75daee1e2ffb00986d15a141612fa34d5033eab
Danilo <danilo.colloca91@gmail.com>
2021-02-08 12:42:21 +0100

    XHR/ e prinnt

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*
+Esercizio di oggi Vue Dischi
+repo/cartella vue-dischi
+Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+Stampiamo tutto a schermo.
+In questo momento non è importante la parte grafica.
+Bonus (per oggi pomeriggio): Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+*/
+
+new Vue({
+	el : '#app',
+	data : {
+	 diskList : []
+	},
+	mounted(){ 
+	 const self = this; 
+	 axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+	 .then(function(queryReturn){
+		console.log(queryReturn.data.response)
+		self.diskList.push(queryReturn.data.response)
+	 });
+	}
+});
+Vue.config.devtools = true
+  
